9a3070446a7c41442fb7259f21cf9ed22e62b531
yll123y <162037721+yll123y@users.noreply.github.com>
2024-03-22 20:14:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import requests
+
+import datetime
+time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
  
 url = "https://push.showdoc.com.cn/server/api/push/c59cbd9bc8f07a5e3bf108ee705588b92019146896"
-myParams = {"title":"username","content":"plusroax"}
+myParams = {"title":"test","content":time}
 res = requests.get(url=url, params=myParams)
  
 print('url:',res.request.url)
